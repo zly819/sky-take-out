10101afcbeb6970d482f3d0cd4d0ee1a93f969bd
zly321 <zhangliye0721@163.com>
2025-07-26 11:10:06 +0800

    新增菜品-文件上传-功能开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,48 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * @author 张立业
+ * @version 1.0
+ */
+@Slf4j
+@RequestMapping("/admin/common")
+@Controller
+public class CommonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
+    //使用阿里云oss上传
+    @PostMapping("/upload")
+    @ResponseBody
+    public Result upload(MultipartFile file) {
+        //获取原始文件名
+        String originalFilename = file.getOriginalFilename();
+        log.info("文件上传，原始文件名：{}", originalFilename);
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String url = null;
+        try {
+            //1.调用AliOssUtil工具类的upload文件上传方法
+            String objectName = UUID.randomUUID().toString() + suffix;
+            url = aliOssUtil.upload(file.getBytes(), objectName);
+        } catch (IOException e) {
+            log.info("文件上传失败！！！{}", e.getMessage());
+            return Result.error("文件上传失败");
+        }
+
+        //2.返回图片路径结果
+        return Result.success(url);
+    }
+}

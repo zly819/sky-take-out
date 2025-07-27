90bdc7e5f55fff95ebcadcb351d8522a57a85991
zly321 <zhangliye0721@163.com>
2025-07-27 16:08:51 +0800

    店铺营业状态设置

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,47 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+/**
+ * @author 张立业
+ * @version 1.0
+ */
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Slf4j
+public class ShopController {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 设置店铺状态 1: 营业 0: 休息
+     *
+     * @param status
+     * @return
+     */
+    @PutMapping("/{status}")
+    public Result<String> setShopStatus(@PathVariable Integer status) {
+        log.info("设置店铺状态: {}", status == 1 ? "营业" : "休息");
+        redisTemplate.opsForValue().set("SHOP_STATUS", status);
+        return Result.success();
+    }
+
+    /**
+     * 获取店铺状态 1: 营业 0: 休息
+     *
+     * @return
+     */
+    @GetMapping("/status")
+    public Result<Integer> getShopStatus() {
+        Integer status = (Integer) Optional.ofNullable(redisTemplate.opsForValue().get("SHOP_STATUS")).orElse(0);
+        log.info("获取店铺状态: {}", status == 1 ? "营业" : "休息");
+        return Result.success(status);
+    }
+}

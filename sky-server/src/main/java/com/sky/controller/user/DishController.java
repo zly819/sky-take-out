86bcc5caa54efe1e2af156af489a119475a5c557
zly321 <zhangliye0721@163.com>
2025-07-29 09:40:40 +0800

    缓存菜品-新增缓存

--- sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,9 @@ public class DishController {
     @Autowired
     private DishService dishService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     /**
      * 根据分类id查询菜品
      *
@@ -31,11 +35,26 @@ public class DishController {
     @GetMapping("/list")
     @ApiOperation("根据分类id查询菜品")
     public Result<List<DishVO>> list(Long categoryId) {
+
+        //缓存优化
+        //查询MySQL数据库前，先判断redis缓存中是否存在数据
+        String key = "dish_" + categoryId;
+        List<DishVO> dishVOList = (List<DishVO>) redisTemplate.opsForValue().get(key);
+        //如果有缓存数据，直接返回
+        if (dishVOList != null && dishVOList.size() > 0) {
+            return Result.success(dishVOList);
+        }
+
+        //如果没有缓存数据，查询MySQL数据库
         Dish dish = new Dish();
         dish.setCategoryId(categoryId);
         dish.setStatus(StatusConstant.ENABLE);//查询起售中的菜品
 
         List<DishVO> list = dishService.listWithFlavor(dish);
+
+        //将MySQL数据库查询到的数据，存入redis缓存中
+        redisTemplate.opsForValue().set(key, list);
+
 
         return Result.success(list);
     }

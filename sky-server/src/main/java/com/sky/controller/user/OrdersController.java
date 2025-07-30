685ed41d10a35d651e28c4af1b519fe14d17187a
zly321 <zhangliye0721@163.com>
2025-07-30 15:23:49 +0800

    订单支付代码导入--未修改

--- sky-server/src/main/java/com/sky/controller/user/OrdersController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrdersController.java
@@ -1,17 +1,16 @@
 package com.sky.controller.user;
 
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.result.Result;
 import com.sky.service.OrdersService;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author ZLY
@@ -37,5 +36,20 @@ public class OrdersController {
         log.info("用户下单：{}",dto);
         OrderSubmitVO vo = ordersService.submit(dto);
         return Result.success(vo);
+    }
+
+    /**
+     * 订单支付
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+    @PutMapping("/payment")
+    @ApiOperation("订单支付")
+    public Result<OrderPaymentVO> payment(@RequestBody OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+        log.info("订单支付：{}", ordersPaymentDTO);
+        OrderPaymentVO orderPaymentVO = ordersService.payment(ordersPaymentDTO);
+        log.info("生成预支付交易单：{}", orderPaymentVO);
+        return Result.success(orderPaymentVO);
     }
 }
--- sky-server/src/main/java/com/sky/mapper/OrdersMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrdersMapper.java
@@ -1,9 +1,24 @@
 package com.sky.mapper;
 
 import com.sky.entity.Orders;
+import org.apache.ibatis.annotations.Select;
 
 public interface OrdersMapper {
 
 
     void insert(Orders orders);
+
+    /**
+     * 根据订单号和用户id查询订单
+     * @param orderNumber
+     * @param userId
+     */
+    @Select("select * from orders where number = #{orderNumber} and user_id= #{userId}")
+    Orders getByNumberAndUserId(String orderNumber, Long userId);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
 }
